767503bc32021ecbb527e28cee4c889ca991c93c
thebkbuffalo <evan.m.berg@gmail.com>
2014-09-24 18:08:08 -0400

    drop password column/add password_digest column not working yet

--- db/migrations/002_change_password_to_password_digest.rb
+++ db/migrations/002_change_password_to_password_digest.rb
@@ -1,6 +1,11 @@
 # db/migrations/002_changes_password_to_password_digest
 
 Sequel.migration do
-  
-  drop_column()
+
+
+    drop_column :users, :password
+
+
+    add_column :users, :password_digest, :size=>225, :null=>false
+
 end # end's sequel.migration
